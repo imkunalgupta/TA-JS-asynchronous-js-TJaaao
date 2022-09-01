de85475ff8c02ac7bd8de95ab7aa7e36ba64ed96
Kunal <mailto.guptakunal@gmail.com>
2022-09-02 00:44:19 +0530

    Spaceflight News App error

--- block-BJaajw/code/index.js
+++ block-BJaajw/code/index.js
@@ -1,77 +1,95 @@
-const ul = document.querySelector('ul');
-const select = document.querySelector('select');
-const divVisible = document.querySelector('div');
-let allNews = [];
+(function () {
+  const url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=30`;
+  const ul = document.querySelector('ul');
+  const error = document.querySelector('.error');
+  const select = document.querySelector('select');
+  const main = document.querySelector('.main');
+  const errorElm = document.querySelector('.error-msg');
+  let allNews = [];
 
-function renderNews(news = []) {
-  ul.innerHTML = '';
-  news.forEach((newsItem) => {
-    let li = document.createElement('li');
-    li.classList.add('flex');
-    let img = document.createElement('img');
-    img.src = newsItem.imageUrl;
-    img.alt = newsItem.title;
-    let div = document.createElement('div');
-    let h4 = document.createElement('h4');
-    h4.innerText = newsItem.newsSite;
-    let p = document.createElement('p');
-    p.innerText = newsItem.title;
-    let a = document.createElement('a');
-    a.href = newsItem.url;
-    let button = document.createElement('button');
-    a.append(button);
-    button.innerText = 'Read More';
-    div.append(h4, p, a);
-    li.append(img, div);
-    ul.append(li);
-  });
-}
-
-function displayOption(sources) {
-  sources.forEach((source) => {
-    let option = document.createElement('option');
-    option.innerText = source;
-    option.value = source;
-    select.append(option);
-  });
-}
-
-const url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=30`;
-fetch(url)
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`Error: ${res.status}`);
-    }
-    return res.json();
-  })
-  .then((news) => {
-    if (!navigator.online) {
-      throw new Error(`Network is not connected!`);
-    }
-    if (divVisible.classList === 'hidden') {
-      divVisible.classList.remove('hidden');
-      divVisible.classList.add('visible');
-    }
-    console.log(news);
-    allNews = news;
-    renderNews(news);
-    let allSources = Array.from(new Set(news.map((n) => n.newsSite)));
-    displayOption(allSources);
-  })
-  .catch((error) => {
-    ul.innerText = error;
-  })
-  .finally(() => {
-    divVisible.classList.add('hidden');
-    divVisible.classList.remove('visible');
-  });
-
-select.addEventListener('change', (event) => {
-  let source = event.target.value.trim();
-  if (source) {
-    var filterdNews = allNews.filter((news) => news.newsSite === source);
-  } else {
-    filterdNews = allNews;
+  function handleErrorMsg(message = 'Something Went Wrong!') {
+    main.style.display = 'none';
+    errorElm.style.display = 'block';
+    errorElm.innerText = message;
   }
-  renderNews(filterdNews);
-});
+
+  function handleSpinner(status = false) {
+    if (status) {
+      ul.innerHTML = `<div class ="spinner"><div class="donut"></div></div>`;
+    }
+  }
+
+  function renderNews(news) {
+    ul.innerHTML = '';
+    news.forEach((newsItem) => {
+      let li = document.createElement('li');
+      li.classList.add('flex');
+      let img = document.createElement('img');
+      img.src = newsItem.imageUrl;
+      img.alt = newsItem.title;
+      let div = document.createElement('div');
+      let h4 = document.createElement('h4');
+      h4.innerText = newsItem.newsSite;
+      let p = document.createElement('p');
+      p.innerText = newsItem.title;
+      let a = document.createElement('a');
+      a.href = newsItem.url;
+      let button = document.createElement('button');
+      a.append(button);
+      button.innerText = 'Read More';
+      div.append(h4, p, a);
+      li.append(img, div);
+      ul.append(li);
+    });
+  }
+
+  function displayOption(sources) {
+    sources.forEach((source) => {
+      let option = document.createElement('option');
+      option.innerText = source;
+      option.value = source;
+      select.append(option);
+    });
+  }
+
+  function init() {
+    handleSpinner(true);
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Response not ok!');
+        }
+      })
+      .then((news) => {
+        handleSpinner();
+        allNews = news;
+        renderNews(news);
+        let allSources = Array.from(new Set(news.map((n) => n.newsSite)));
+        displayOption(allSources);
+      })
+      .catch((error) => {
+        handleErrorMsg(error);
+      })
+      .catch((error) => {
+        handleSpinner();
+      });
+  }
+
+  select.addEventListener('change', (event) => {
+    let source = event.target.value.trim();
+    if (source) {
+      var filterdNews = allNews.filter((news) => news.newsSite === source);
+    } else {
+      filterdNews = allNews;
+    }
+    renderNews(filterdNews);
+  });
+
+  if (!navigator.online) {
+    init();
+  } else {
+    handleErrorMsg('check your internet connection :crossed_swords:');
+  }
+})();
